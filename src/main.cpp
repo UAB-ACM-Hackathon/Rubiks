f3757e34713dae55292144c5a993dd5017406c62
qualityplease <bryant.evan.a@gmail.com>
2014-03-01 12:30:24 -0600

    test

--- src/main.cpp
+++ src/main.cpp
@@ -20,14 +20,34 @@ using std::string;
 #include <assert.h>
 #define _USE_MATH_DEFINES
 
+GLfloat zoom, rotx, roty, rotz, transx, transy, transz;
+bool* keys;
 // Cube cube;
+
+// viewport matrix
+GLint view[4]  = { 0, 0, 750, 750 };
 
 void gfxinit()
 {
-	// cube = Cube();
+  glEnable( GL_DEPTH_TEST );
+  glClearColor( 0.0, 0.0, 0.0, 1.0 );
+  glEnable( GL_LINE_SMOOTH );
+  glHint( GL_LINE_SMOOTH_HINT, GL_NICEST );
+  glEnable( GL_POINT_SMOOTH );
+  glHint( GL_POINT_SMOOTH_HINT, GL_NICEST );
+  glPointSize( 3.0 );
+  glLineWidth( 1.5 );
+
+  glShadeModel( GL_SMOOTH );
+
+  zoom = 1.0;
+  rotx = 0.0; roty = 0.0; rotz = 0.0;
+  transx = 0.0; transy = 0.0; transz = 0.0;
+
+  keys = new bool[256];
 }
 
-void reshape(int w, int h)
+void reshape( int w, int h )
 {
   glViewport( 0, 0, w, h );
 
@@ -45,9 +65,69 @@ void reshape(int w, int h)
   glLoadIdentity();
 }
 
+void keyboard( unsigned char key, int x, int y )
+{
+  switch ( key )
+  {
+  case 27: exit( 1 ); break;
+  default:            break;
+  }
+
+	cout << "normal: " << key << endl;
+
+  glutPostRedisplay();
+}
+
+void specialDown( int key, int x, int y )
+{
+	cout << "special: " << key << endl;
+
+  glutPostRedisplay();
+}
+
+void specialUp( int key, int x, int y )
+{
+  glutPostRedisplay();
+}
+
 void display()
 {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+  glMatrixMode(GL_MODELVIEW);
+  glPushMatrix();
+/*
+  glScalef( zoom, zoom, zoom );
+  glRotatef( rotx, 1.0, 0.0, 0.0 );
+  glRotatef( roty, 0.0, 1.0, 0.0 );
+  glRotatef( rotz, 0.0, 0.0, 1.0 );
+*/
+
+  // draw objects
+
+  glBegin( GL_TRIANGLES );
+
+  glColor3f( 1, 0, 0 );
+  glVertex3f( 0., 0., 0. ); glVertex3f( .5, 0., 1. ); glVertex3f( 1., 0., 0. );
+  glColor3f( 0, 1, 0 );
+  glVertex3f( 0., 0., 0. ); glVertex3f( 1., 0., 0. ); glVertex3f( .5, .5, 0. );
+  glColor3f( 0, 0, 1 );
+  glVertex3f( 0., 0., 0. ); glVertex3f( .5, .5, 0. ); glVertex3f( .5, 0., 1. );
+  glColor3f( 1, 1, 0 );
+  glVertex3f( 1., 0., 0. ); glVertex3f( .5, 0., 1. ); glVertex3f( .5, .5, 0. );
+
+  glEnd();
+
+  glPopMatrix();
+
+  glFlush();
+  glutSwapBuffers();
+}
+
+void timer( int value )
+{
+  glutTimerFunc( 25, timer, 0 );
+  glutPostRedisplay();
 }
 
 int main( int argc, char **argv )
